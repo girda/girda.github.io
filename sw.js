aa407f5333d125972535a3629825bd69ddd63170
ArtG <artem.girda@gmail.com>
2019-05-07 16:21:01 +0300

    test

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
 const staticAssets = [
     './index.html',
-    './css/app.css',
-    './js/app.js',
 ]
 
 self.addEventListener('install', async event => {
     const cache = await caches.open('news-static');
     cache.addAll(staticAssets)
-    console.log(staticAssets)
 });
-console.log('sw-test')
+
 self.addEventListener('fetch', event => {
     const req = event.request;
     const url = new URL(req.url)
     
-    console.log(req)
     if ( url.origin === location.origin ) {
         event.respondWith(cacheFirst(req));
     } else {
@@ -24,7 +20,5 @@ self.addEventListener('fetch', event => {
 
 async function cacheFirst(req) {
     const cachedResponse = await caches.match(req);
-    console.log(cachedResponse)
-    console.log(fetch(req))
     return cachedResponse || fetch(req)
 }
